8207140bae810c25aba3f024573814603d1343b3
LaurentLienhard <laurent.lienhard@outlook.com>
2022-03-27 18:52:09 +0200

    update

--- Sources/Ressources/DockerEnv/MariaDB/DEV/backend/run.sh
+++ Sources/Ressources/DockerEnv/MariaDB/DEV/backend/run.sh
@@ -5,5 +5,10 @@ service nginx start
 service mariadb start
 mysql -uroot -proot123 < configure-mariadb.sql
 
+
 ./vendor/bin/phinx migrate
+
+./bin/cli env:create --name production --type MariaDB --host localhost --username root --password root123 --databasename fusionsuite_db --current
+./bin/cli install
+
 tail -f /var/log/nginx/error.log
